2d4789a69266e82dfe99486e7fb2d9f6f1aeacf7
Sajid Ali <sajid.ali@phaedrasolutions.com>
2019-08-01 20:11:23 +0500

    minor changes in UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux'
 import React, { Component } from 'react';
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView, NetInfo, Alert } from 'react-native';
 
 import AppContainer from './navigators';
 import configureStore from './store';
@@ -10,10 +10,38 @@ import { axiosClient } from './utils/config'
 const store = configureStore();
 
 export default class App extends Component {
+
+
+  state = {
+    connection_Status: "",
+    isConnected: true,
+  }
+
+  componentDidMount () {
+    NetInfo.isConnected.addEventListener(
+      'connectionChange',
+      this._handleConnectivityChange
+    );
+  }
+
+  _handleConnectivityChange = (isConnected) => {
+    this.setState({ isConnected });
+  };
+
   render () {
     return (
       <Provider store={store}>
         <SafeAreaView style={{ flex: 1 }}>
+          {!this.state.isConnected ? Alert.alert(
+            "No Internet Access",
+            "You do not have Internet Access. Please connect to internet in order to use the App.",
+            [
+              {
+                text: 'OK', onPress: () => {
+                }
+              }
+            ]
+          ) : null}
           <AppContainer />
         </SafeAreaView>
       </Provider>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -13,5 +13,5 @@ const getAuthToken = async () => {
 
 // Network constants
 // export const baseURL = 'https://tranquil-anchorage-35603.herokuapp.com/api/v1';
-export const baseURL = 'http://104.248.74.175:3000/api/v1';
+export const baseURL = 'http://foodallinone.com/api/v1';
 export const token = getAuthToken();
